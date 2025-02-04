3c765d4f545cb73aff2bb50d5bf350dbf703d1f0
lmespino01~ <lmespino@cps.edu>
2025-02-04 15:11:40 -0600

    first commit

---
+++ Performance.py
@@ -0,0 +1,12 @@
+# restaurant program: Asks the user a selection/list of types foods that they would like to eat (this will help decide what restaurant they will decide to go to.), afterwards, they pick the restaurant and get the menu, with the menu in hand they order what they would
+# like to eat, after everything is order they get the bill, and then are asked to rate the restaurant and if they would like to recommend the restaurant to a friend or not.
+
+# To do list:
+#   List of types of foods to decide restaurant
+#   List of restaurants
+#   Food menu for restaurants
+#   Food bill/price
+#   Restaurant rating
+#   Recommend restaurant
+
+Type_of_restaurant = []
